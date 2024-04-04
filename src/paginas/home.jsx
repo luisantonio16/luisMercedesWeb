f0bf34a30fce0eb0df3d37afba9cb9ccdfc0d79e
¨luisantonio16¨ <luis16.16m@gmail.com>
2024-04-04 11:26:14 -0400

    update home

--- src/paginas/home.jsx
+++ src/paginas/home.jsx
@@ -8,7 +8,7 @@ function Home() {
     
     return (
         <>
-        <div>
+        <div className='home-titulos'> 
               <h1>Luis Mercedes Web</h1>
               <p>Desarrollador de Software</p>
         </div>
